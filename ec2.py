7cc626e7fdfd9dba35198a8e5e703e6a354bfecd
sachin-tech <51818996+sachin-tech@users.noreply.github.com>
2022-09-13 21:47:48 +0530

    Update ec2.py

--- ec2.py
+++ ec2.py
@@ -2,6 +2,7 @@ import boto3
 ec2 = boto3.resource('ec2')
 instances = ec2.create_instances(
         ImageId="ami-05fa00d4c63e32376",
+        VPCId="vpc-06e2337457b5130b5",
         MinCount=1,
         MaxCount=1,
         InstanceType="t2.micro",
