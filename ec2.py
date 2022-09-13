6fbf1cb03b452f7b39377f76ebf6182238eaf4f1
sachin-tech <51818996+sachin-tech@users.noreply.github.com>
2022-09-13 21:54:20 +0530

    Update ec2.py

--- ec2.py
+++ ec2.py
@@ -1,8 +1,11 @@
 import boto3
+conn = boto3.client('conn', 
+                    'us-east-1',
+                     aws_access_key_id='AKIA4RH4TEY6T7H65CPW',
+                     aws_secret_access_key='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')
 ec2 = boto3.resource('ec2')
 instances = ec2.create_instances(
         ImageId="ami-05fa00d4c63e32376",
-        VPCId="vpc-06e2337457b5130b5",
         MinCount=1,
         MaxCount=1,
         InstanceType="t2.micro",
