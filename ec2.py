42a2896ea0592417e48ef0c85dc40d657fbaf1f6
sachin-tech <51818996+sachin-tech@users.noreply.github.com>
2022-09-14 19:43:55 +0530

    Update ec2.py

--- ec2.py
+++ ec2.py
@@ -1,10 +1,10 @@
-#CReating ec2 instance
+CReating ec2 instance
 import boto3
-#client = boto3.client(
- #   'ec2',
-  #  aws_access_key_id="AKIA4RH4TEY6T7H65CPW",
-   # aws_secret_access_key="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"
-#)
+client = boto3.client(
+    'ec2',
+    aws_access_key_id="AKIA4RH4TEY6T7H65CPW",
+    aws_secret_access_key="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"
+)
 ec2 = boto3.resource('ec2')
 instances = ec2.create_instances(
         ImageId="ami-05fa00d4c63e32376",
