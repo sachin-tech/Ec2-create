30474ea9381436b636b54b252ca8d7d778a1c35b
sachin-tech <51818996+sachin-tech@users.noreply.github.com>
2022-09-14 23:17:29 +0530

    Update describe-delete.py

--- describe-delete.py
+++ describe-delete.py
@@ -1,4 +1,4 @@
 import boto3
-ec2 = boto3.client('ec2')
-myinstance = ec2.describe_instances(Filters=[{'Name': 'Name', 'Values': ['Jenkins_Built_instance']}]
+ec2 = boto3.resource('ec2', 'us-east-1')
+myinstance = c2.instances.filter(Filters=[{'Name': 'Name', 'Values': ['Jenkins_Built_instance']}]
 print(myinstance)
