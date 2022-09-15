98cbccc40e340dd920223d795404b43a31b731dd
sachin-tech <51818996+sachin-tech@users.noreply.github.com>
2022-09-15 19:58:39 +0530

    Update describe-delete.py

--- describe-delete.py
+++ describe-delete.py
@@ -1,10 +1,5 @@
 import boto3
-#ec2 = boto3.client('ec2')
-#myinstance = ec2.describe_instances()
-ec2 = boto3.resource('ec2')
-instancename = ''
-for tags in ec2.instances.tags:
-   if tags[“Key”] == ‘Name’:
-      instancename = tags[“Value”]
-      print(instancename)
+ec2 = boto3.client('ec2')
+myinstance = ec2.describe_instances()
+print(myinstance)
 
