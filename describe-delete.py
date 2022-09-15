a9b16aac3e98ec9450185de99b06bbae378cb0f5
sachin-tech <51818996+sachin-tech@users.noreply.github.com>
2022-09-15 19:32:11 +0530

    Update describe-delete.py

--- describe-delete.py
+++ describe-delete.py
@@ -1,11 +1,10 @@
 import boto3
 #ec2 = boto3.client('ec2')
 #myinstance = ec2.describe_instances()
-ec2 = boto3.resource('ec2',"us-east-1")
-instances = ec2.instances.filter(Filters=[{'Name': 'Name', 'Values': ['Jenkins_Built_instance']}])
-for instance in instances:
-   if instance.tags != None:
-      for tags in instance.tags:
-        if tags["Key"] == 'Name':
-          instancename = tags["Value"]
-          print("Inastance Name - %s,  Instance Id - %s, Owner - %s " %(instancename,instance.id,owner))
+ec2 = boto3.resource('ec2')
+instancename = ''
+for tags in ec2instance.tags:
+   if tags[“Key”] == ‘Name’:
+      instancename = tags[“Value”]
+      print(instancename)
+
