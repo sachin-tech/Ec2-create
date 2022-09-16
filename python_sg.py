fd5188727d53fe1ec27dfe08e079c749b51f6355
sachin-tech <51818996+sachin-tech@users.noreply.github.com>
2022-09-16 22:11:50 +0530

    Update python_sg.py

--- python_sg.py
+++ python_sg.py
@@ -1,15 +1,16 @@
 import boto3
 from botocore.exceptions import ClientError
+ec2 = boto3.client('ec2')
 AWS_REGION = 'us-east-1'
 vpc_resource = boto3.resource("ec2", region_name=AWS_REGION)
 response = boto3.describe_vpcs()
 vpc_id = response.get('Vpcs', [{}])[0].get('VpcId', '')
 try:
-      response = ec2.create_securitygroup(GroupName='python_sg', Description='created using python', VpcId=vpc_id, region='us-east-1')
+      response = vpc_resource.create_securitygroup(GroupName='python_sg', Description='created using python', VpcId=vpc_id, region='us-east-1')
       security_group_id = response['GroupId']
       print('Security Group Created %s in vpc %s.' % (security_group_id, vpc_id))
       
-      data = ec2.authorize_security_group_ingress(
+      data = vpc_resource.authorize_security_group_ingress(
         GroupId = security_group_id,
         IpPermission=[
            {'IpProtocol': 'tcp',
