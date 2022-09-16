dd59530836ca1eb442e87fced8466ed7c526ee39
sachin-tech <51818996+sachin-tech@users.noreply.github.com>
2022-09-16 19:07:41 +0530

    Update create_S3_Bucket.py

--- create_S3_Bucket.py
+++ create_S3_Bucket.py
@@ -1,5 +1,5 @@
 import boto3
-resource = boto3.resource("s3"))
+resource = boto3.resource("s3")
 bucket_name = "python_created_bucket"
 location = {'LocationConstraint'}
 bucket = resource.create_bucket(
