4f9c0815e794abcfaf79e38830810554de259030
sachin-tech <51818996+sachin-tech@users.noreply.github.com>
2022-09-16 20:58:48 +0530

    Create python_sg.py

---
+++ python_sg.py
@@ -0,0 +1,23 @@
+import boto3
+ec2 = boto3.client('ec2')
+response = boto3.describe_vpcs()
+vpc_id = response.get('Vpcs', [{}][0].get('VpcId', '')
+try:
+      response = ec2.create_securitygroup(GroupName='python_sg' Description='created using python', VpcId=vpc_id)
+      security_group_id = response['GroupId']
+      print('Security Group Created %s in vpc %s.' % (security_group_id, vpc_id))
+      
+      data = ec2.authorize_security_group_ingress(
+        GroupId = security_group_id,
+        IpPermission=[
+           {'IpProtocol': 'tcp',
+            'FromPort': 80,
+            'ToPort': 80,
+            'IpRanges': [{CidrIp': '0.0.0.0/0'}]},
+            'IpProtocol': 'tcp',
+            'FromPort': 22,
+            'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
+        ])
+      print('Ingress Successfully Set %s' % data)
+except ClientError as e:
+ print(e)            
